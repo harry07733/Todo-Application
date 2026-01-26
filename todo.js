db32ab9c1b576f723f79eaf6b6a85f383258a5ca
harry07733 <shaswatswain53@gmail.com>
2026-01-26 20:01:01 +0530

    Updated todo application logic and UI

--- todo.js
+++ todo.js
@@ -15,11 +15,36 @@ let todoList = [
     }
 ];
 
+let todoCount = todoList.length;
+
+function onTodoStatusChanged(checkboxId, labelId) {
+    // let checkboxElement = document.getElementById(checkboxId);
+    let labelElement = document.getElementById(labelId);
+    // if(checkboxElement.checked === true){
+    //     labelElement.classList.add("checked");
+    // }
+    // else{
+    //     labelElement.classList.remove("checked");
+    // }
+
+    labelElement.classList.toggle("checked");
+}
+
+function onDeleteTodo(todoId) {
+    let todoElement = document.getElementById(todoId);
+
+    todoItemsContainer.removeChild(todoElement);
+}
+
 function createAndAppendTodo(todo) {
     let checkboxId = "checkbox" + todo.uniqueNo;
+    let labelId = "label" + todo.uniqueNo;
+    let todoId = "todo" + todo.uniqueNo;
+
     // For li
     let todoElement = document.createElement("li");
     todoElement.classList.add("todo-item-container", "d-flex", "flex-row");
+    todoElement.id = todoId;
     todoItemsContainer.appendChild(todoElement);
 
     // For input
@@ -27,6 +52,11 @@ function createAndAppendTodo(todo) {
     inputElement.type = "checkbox";
     inputElement.id = checkboxId;
     inputElement.classList.add("checkbox-input");
+
+    inputElement.onclick = function() {
+        onTodoStatusChanged(checkboxId, labelId);
+    }
+
     todoElement.appendChild(inputElement);
 
     // For div
@@ -40,6 +70,7 @@ function createAndAppendTodo(todo) {
     labelElement.setAttribute("for", checkboxId);
     labelElement.classList.add("checkbox-label");
     labelElement.textContent = todo.text ;
+    labelElement.id = labelId;
     labelContainer.appendChild(labelElement);
 
     // For div
@@ -50,6 +81,9 @@ function createAndAppendTodo(todo) {
     // For i
     let iconElement = document.createElement("i");
     iconElement.classList.add("fa-solid", "fa-trash", "delete-icon");
+    iconElement.onclick = function() {
+        onDeleteTodo(todoId);
+    }
     deleteIconContainer.appendChild(iconElement);
 }
 // createAndAppendTodo(todoList[0]);
@@ -60,3 +94,26 @@ for(let todo of todoList){
     createAndAppendTodo(todo);
 }
 
+
+function onAddTodo() {
+    let userInputElement = document.getElementById("todoUserInput");
+    let userInputValue = userInputElement.value; 
+    if(userInputValue === ""){
+        alert("Enter a valid text!!");
+        return;
+    }
+    todoCount += 1;
+
+    let newTodo = {
+        text: userInputValue,
+        uniqueNo: todoCount
+    }
+
+    createAndAppendTodo(newTodo);
+    userInputElement.value = "";
+}
+
+let addTodoButton = document.getElementById("addTodoButton");
+addTodoButton.onclick = function() {
+    onAddTodo();
+}
